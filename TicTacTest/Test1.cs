754e2dc77c6415066d458316a75b0f9f67cc8229
KornienkoVl <114586091+KornienkoVl@users.noreply.github.com>
2025-03-24 13:21:09 +0700

    Начало проекта
    Создан игровой класс с матрицей для игры. Тест для метода хода игрока. Сам метод - заглушка.
    TODO:
    1)Метод хода игрока на клетку
    2)Добавление проверки на возможность хода (на пустоту клетки)
    3)Добавление метода поочередной смены фигур игроков (Ход Х -> Ход О -> Ход Х) (Отображение в матрице игры Х - 1, О - (-1))
    4)Метод проверки на победу по горизонталям, вертикалям, диагоналям + проверка на ничью
    5)Метод подсчета количества побед фигур
    6)Метод записи игровой статистики в файл
    7)Метод загрузки игровой статистики из файла
    8)Метод очистки игровой статистики

---
+++ TicTacTest/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace TicTac.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Game game = new Game();
+            Assert.IsTrue(game.MakeMove(1));
+        }
+    }
+}
